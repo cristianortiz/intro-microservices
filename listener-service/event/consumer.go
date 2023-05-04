63be60f23f777651ff5784ae561954f2e122ca1e
cristian.ortiz@orbisdata.cl <cristian.ortiz@orbisdata.cl>
2023-05-03 22:15:24 -0400

    - packge listenet, to implement rabbitMQ and connect with the broker and the other services

---
+++ listener-service/event/consumer.go
@@ -0,0 +1,114 @@
+package event
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Consumer struct {
+	conn      *amqp.Connection
+	queueName string
+}
+
+func NewConsumer(conn *amqp.Connection) (Consumer, error) {
+
+	consumer := Consumer{
+		conn: conn,
+	}
+	err := consumer.setup()
+	if err != nil {
+		return Consumer{}, err
+	}
+	return consumer, nil
+}
+
+func (consumer *Consumer) setup() error {
+
+	channel, err := consumer.conn.Channel()
+	if err != nil {
+		return err
+	}
+	//create and exchange in amqp server
+	return declareExchange(channel)
+
+}
+
+// push events to amqp server
+type Payload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
+func (consumer *Consumer) Listen(topics []string) error {
+	ch, err := consumer.conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	q, err := declareRandomQueue(ch)
+	if err != nil {
+		return err
+	}
+
+	for _, s := range topics {
+		//bind the opened channel with the received topics
+		err := ch.QueueBind(
+			q.Name,
+			s,            //a topic
+			"logs_topic", //custom topic's name
+			false,        //no wait
+			nil,
+		)
+		if err != nil {
+			return err
+		}
+	}
+
+	//check for messages in the queue
+	messages, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	//consume all the messages from rabbit until the app is closed
+	forever := make(chan bool)
+	go func() {
+		for d := range messages {
+			var payload Payload
+			_ = json.Unmarshal(d.Body, &payload)
+			go handlePayload(payload)
+		}
+	}()
+
+	fmt.Printf("waiting for messages [Exchange, Queue] [logs_topic, %s]\n", q.Name)
+	<-forever
+
+	return nil
+}
+
+func handlePayload(payload Payload) {
+	switch payload.Name {
+	case "log", "event":
+		//log whatever we get
+		err := logEvent(payload)
+		if err != nil {
+			log.Println(err)
+		}
+	case "auth":
+		//authenticate
+
+	default:
+		err := logEvent(payload)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+}
+func logEvent(paylod Payload) error {
+	return nil
+}
